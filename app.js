86c25c86184e099c2849f0abac65ff7be97cc156
frankiescott788 <scottfrankie864@gmail.com>
2024-10-07 15:26:54 +0200

    updated

--- app.js
+++ app.js
@@ -13,7 +13,37 @@ http.createServer( (req, res) => {
         })
     }
     if(req.url === '/addData') {
+        fs.readFile('data.json', (err, data) => {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            let jsonData;
+            try {
+                jsonData = JSON.parse(data.toString());
+            } catch (err) {
+                console.log('Error parsing JSON:', err);
+                return;
+            }
+    
+            jsonData.push({
+                "id": 4,
+                "name": "Butter",
+                "quantity": 1
+            });
+    
+            fs.writeFile('data.json', JSON.stringify(jsonData, null, 2), (err) => {
+                if(err) {
+                    console.log(err);
+                    return;
+                }
+                console.log('file written');
+            });
+        });
+    }
+    if(req.url === '/delete:id') {
         
-    } 
+    }
+    
 })
 .listen(8080)
